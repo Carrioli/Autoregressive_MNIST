ccc17386265d9d47a56fe76cadbdc97d71f35615
Damiano Carrioli <damiano.carrioli@gmail.com>
2024-04-14 17:38:06 -0700

    better hyperparameters

--- training.py
+++ training.py
@@ -140,9 +140,9 @@ def main(train_loader, test_loader, params, opt_state):
 
 
 n_outer_blocks = 1
-n_transformers = 16
-n_blocks       = 4
-n_heads        = 12
+n_transformers = 36
+n_blocks       = 2
+n_heads        = 36
 num_classes    = 256 # same as d_out
 d_model        = 64 # same as feature size
 d_qk           = 8
