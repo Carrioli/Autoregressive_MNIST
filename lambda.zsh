3f75c248cbf6bbf432be07a261df08f0429c3d33
Damiano Carrioli <damiano.carrioli@gmail.com>
2024-05-10 15:42:25 -0700

    updating zsh script

--- lambda.zsh
+++ lambda.zsh
@@ -1,4 +1,3 @@
 pip3 install --upgrade pip
 pip3 install optax jaxtyping
-pip3 install -U --pre jax -f https://storage.googleapis.com/jax-releases/jax_nightly_releases.html
-pip3 install -U --pre jaxlib -f https://storage.googleapis.com/jax-releases/jaxlib_nightly_cuda12_releases.html
+pip3 install --upgrade "jax[cuda12]"
